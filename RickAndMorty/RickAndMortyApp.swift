4f941b516454e1bef63e9d2d2aa8955c7fa43491
Kevin Trujillo <kevin.trujillo@grupohasten.com>
2023-07-25 23:24:06 +0100

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Juan Kevin Trujillo on 25/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
