2b180d008752f4afb0d4cc776fc8fa999c75886b
Juan Kevin Trujillo <me@juankevintrujillo.com>
2023-10-31 15:34:49 +0000

    Add tabBar view and use it on MainView

---
+++ RickAndMorty/App/Views/Common/TabBar/TabBarView.swift
@@ -0,0 +1,40 @@
+//
+//  TabBarView.swift
+//  RickAndMorty
+//
+//  Created by Juan Kevin Trujillo on 31/10/23.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            // Add tab bar items here
+            Text("Tab 1")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("view_tabbar_home")
+                }
+                .tag(0)
+
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "person.3.fill")
+                    Text("view_tabbar_characters")
+                }
+                .tag(1)
+
+            Text("Tab 3")
+                .tabItem {
+                    Image(systemName: "film.fill")
+                    Text("view_tabbar_episodes")
+                }
+                .tag(2)
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+}
