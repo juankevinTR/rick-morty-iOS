d5944f7e81d7107691be238dee47298a44e0fd40
Juan Kevin Trujillo <me@juankevintrujillo.com>
2023-11-06 13:57:23 +0000

    Fix Episode coding keys

--- RickAndMorty/App/Networking/Endpoints/SingleEpisode/ResponseModel/SingleEpisodeResponse.swift
+++ RickAndMorty/App/Networking/Endpoints/SingleEpisode/ResponseModel/SingleEpisodeResponse.swift
@@ -14,8 +14,15 @@ struct SingleEpisodeResponse: Codable {
     let airDate: String?
     let episode: String?
     let characters: [String]?
+
     let url: String?
     let created: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case airDate = "air_date"
+        case episode, characters, url, created
+    }
 }
 
 // MARK: - Mock
