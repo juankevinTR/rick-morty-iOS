fd92a9571258db20ea62adfcd18c44d4c53d58be
Juan Kevin Trujillo <me@juankevintrujillo.com>
2023-11-06 11:59:54 +0000

    Extract season and episode numbers

--- RickAndMorty/App/Models/Episode/Episode.swift
+++ RickAndMorty/App/Models/Episode/Episode.swift
@@ -12,7 +12,8 @@ struct Episode: Codable, Hashable {
     let id: Int
     let name: String
     let airDate: String
-    let episode: String
+    let seasonNumber: Int?
+    let episodeNumber: Int?
     let characters: [Int]? // Better the ids for future searching
     let url: String
     let created: String
@@ -23,8 +24,9 @@ struct Episode: Codable, Hashable {
         return Episode(
             id: response.id ?? -1,
             name: response.name ?? "",
-            airDate: response.airDate ?? "",
-            episode: response.episode ?? "",
+            airDate: response.airDate ?? "", 
+            seasonNumber: getSeasonNumber(from: response.episode),
+            episodeNumber: getEpisodeNumber(from: response.episode),
             characters: response.characters?.compactMap({ characterURL in
                 self.extractCharacterId(from: characterURL)
             }),
@@ -35,6 +37,24 @@ struct Episode: Codable, Hashable {
 }
 
 private extension Episode {
+    private static func getSeasonNumber(from input: String?) -> Int? {
+        guard let input = input, let range = input.range(of: "S") else {
+            return nil
+        }
+        let startIndex = input.index(after: range.upperBound)
+        let seasonNumber = input[startIndex...]
+        return Int(seasonNumber)
+    }
+
+    private static func getEpisodeNumber(from input: String?) -> Int? {
+        guard let input = input, let range = input.range(of: "E") else {
+            return nil
+        }
+        let startIndex = input.index(after: range.upperBound)
+        let episodeNumber = input[startIndex...]
+        return Int(episodeNumber)
+    }
+
     private static func extractCharacterId(from urlString: String) -> Int? {
         if let characterIdRange = urlString.range(of: "/character/") {
             let characterIdSubstring = urlString.suffix(from: characterIdRange.upperBound)
@@ -52,11 +72,25 @@ extension Episode {
         return Episode(
             id: 0,
             name: "Mock Episode",
-            airDate: "",
-            episode: "ME01",
+            airDate: "", 
+            seasonNumber: 1,
+            episodeNumber: 1,
             characters: [1, 5, 8, 10, 12, 16],
             url: "https://www.linkedin.com/in/juankevintrujillo/",
             created: "1992-09-21T00:00:00.000Z"
+        )
+    }
+
+    static func getMockOriginalData() -> Episode {
+        return Episode(
+            id: 28,
+            name: "The Ricklantis Mixup",
+            airDate: "September 10, 2017", 
+            seasonNumber: 3,
+            episodeNumber: 7,
+            characters: [1, 2],
+            url: "https://rickandmortyapi.com/api/episode/28",
+            created: "2017-11-10T12:56:36.618Z"
         )
     }
 }
